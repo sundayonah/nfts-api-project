32b2808abaf0719897162f444d114ffe5b20e106
sundayonah <sundayonah94@gmail.com>
2023-09-16 10:49:51 +0100

    done with web3

--- web3/contracts/nftsIPFS.sol
+++ web3/contracts/nftsIPFS.sol
@@ -2,5 +2,130 @@
 pragma solidity ^0.8.9;
 
 contract nftsIPFS {
+    address payable contractOwner =
+        payable(0x9756B7048Be34e704C27DeEb7dB34BE1A910aB92);
+    uint256 public listingPrice = 0.25 ether;
 
+    struct NFTs {
+        string title;
+        string description;
+        string email;
+        string category;
+        uint256 fundraised;
+        address creator;
+        string image;
+        uint256 timestamp;
+        uint256 id;
+    }
+
+    mapping(uint256 => NFTs) public nftImages;
+
+    uint256 public imagesCount = 0;
+
+    function uploadIPFS(
+        address _creator,
+        string memory _image,
+        string memory _title,
+        string memory _description,
+        string memory _email,
+        string memory _category
+    )
+        public
+        payable
+        returns (
+            string memory,
+            string memory,
+            string memory,
+            address,
+            string memory
+        )
+    {
+        imagesCount++;
+        NFTs storage nft = nftImages[imagesCount];
+
+        nft.title = _title;
+        nft.creator = _creator;
+        nft.description = _description;
+        nft.email = _email;
+        nft.category = _category;
+        nft.image = _image;
+        nft.timestamp = block.timestamp;
+        nft.id = imagesCount;
+
+        return (_title, _description, _category, _creator, _image);
+    }
+
+    function getAllNFTs() public view returns (NFTs[] memory) {
+        uint256 itemCount = imagesCount;
+        uint256 currentIndex = 0;
+
+        NFTs[] memory items = new NFTs[](itemCount);
+        for (uint256 i = 0; i < itemCount; i++) {
+            uint256 currentId = i + 1;
+            NFTs storage currentItem = nftImages[currentId];
+            items[currentIndex] = currentItem;
+            currentIndex += 1;
+        }
+        return items;
+    }
+
+    function getImages(
+        uint256 id
+    )
+        external
+        view
+        returns (
+            string memory,
+            string memory,
+            string memory,
+            string memory,
+            uint256,
+            address,
+            string memory,
+            uint256,
+            uint256
+        )
+    {
+        NFTs memory nfts = nftImages[id];
+        return (
+            nfts.title,
+            nfts.description,
+            nfts.email,
+            nfts.category,
+            nfts.fundraised,
+            nfts.creator,
+            nfts.image,
+            nfts.timestamp,
+            nfts.id
+        );
+    }
+
+    // Updates the listing Price of the contract
+    function updateListingPrice(
+        uint256 _listingPrice,
+        address owner
+    ) public payable {
+        require(
+            contractOwner == owner,
+            "Only contractowner can update listing price"
+        );
+        listingPrice - _listingPrice;
+    }
+
+    // Donate Function
+    function donateToImage(uint256 _id) public payable {
+        uint256 amount = msg.value;
+        NFTs storage nft = nftImages[_id];
+        (bool sent, ) = payable(nft.creator).call{value: amount}("");
+        if (sent) {
+            nft.fundraised = nft.fundraised + amount;
+        }
+    }
+
+    function withdrwal(address _owner) external {
+        require(_owner == contractOwner, "Only owner can withdraw");
+        uint256 balance = address(this).balance;
+        require(balance > 0, "No funds available");
+        contractOwner.transfer(balance);
+    }
 }
--- web3/hardhat.config.js
+++ web3/hardhat.config.js
@@ -1,25 +1,26 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 
-const PRIVATE_KEY = "YOUR PRIVATE KEY";
-const RPC_URL = "https://rpc.ankr.com/polygon_mumbai";
+const PRIVATE_KEY =
+   '1f1aec904899569d60605bfdb13428c689bdce679f31a72ea4c8a13ea5d31cf7';
+const RPC_URL = 'https://rpc.ankr.com/polygon_mumbai';
 module.exports = {
-  defaultNetwork: "polygon_mumbai",
-  networks: {
-    hardhat: {
-      chainId: 80001,
-    },
-    polygon_mumbai: {
-      url: "https://rpc.ankr.com/polygon_mumbai",
-      accounts: [`0x${PRIVATE_KEY}`],
-    },
-  },
-  solidity: {
-    version: "0.8.9",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200,
+   defaultNetwork: 'polygon_mumbai',
+   networks: {
+      hardhat: {
+         chainId: 80001,
       },
-    },
-  },
+      polygon_mumbai: {
+         url: 'https://rpc.ankr.com/polygon_mumbai',
+         accounts: [`0x${PRIVATE_KEY}`],
+      },
+   },
+   solidity: {
+      version: '0.8.9',
+      settings: {
+         optimizer: {
+            enabled: true,
+            runs: 200,
+         },
+      },
+   },
 };
